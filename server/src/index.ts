86519651547577f834e4aca83e3da8215102203a
gmiak <kayss.g@gmail.com>
2022-06-14 20:41:06 +0200

    Client init

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,7 +4,7 @@ import express, { Express, Request, Response } from "express";
 const app : Express = express();
 app.get("/",
   async (req : Request, res : Response) => {
-    res.send("Hello World!");
+    res.send("Hello World! Gmiak");
   });
 app.listen(8080);
 
